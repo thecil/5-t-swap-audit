14e25c29edd6f3a6fe86d0cccd5aa545df416681
thecil <thecilsurf@gmail.com>
2025-09-05 12:06:37 -0400

    copy/paste invariant
    Trying to execute the `invariant_deltaYFollowsMath` or `invariant_deltaXFollowsMath` does not break the protocol invariant at 10 swaps, which should be failing at the 10th swap.

--- test/invariant/Invariant.t.sol
+++ test/invariant/Invariant.t.sol
@@ -1,24 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import { Test } from "forge-std/Test.sol";
-import { StdInvariant } from "forge-std/StdInvariant.sol";
-import { ERC20Mock } from "../mocks/ERC20Mock.sol";
+import { Test, StdInvariant, console2 } from "forge-std/Test.sol";
 import { PoolFactory } from "../../src/PoolFactory.sol";
 import { TSwapPool } from "../../src/TSwapPool.sol";
-import { Handler } from "./Handlers.t.sol";
+import { ERC20Mock } from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import { TSwapPoolHandler } from "./TSwapPoolHandler.sol";
 
 contract Invariant is StdInvariant, Test {
-    Handler handler;
-    // these pools have 2 assets
+    PoolFactory factory;
+    TSwapPool pool;
     ERC20Mock poolToken;
     ERC20Mock weth;
-    // we are gonna need the contracts
-    PoolFactory factory;
-    TSwapPool pool; // poolToken/weth
+    ERC20Mock tokenB;
 
-    int256 constant STARTING_X = 100e18; // Starting ERC20 / poolToken
-    int256 constant STARTING_Y = 50e18; // Starting WETH amount
+    int256 constant STARTING_X = 100e18; // starting ERC20
+    int256 constant STARTING_Y = 50e18; // starting WETH
+    uint256 constant FEE = 997e15; //
+    int256 constant MATH_PRECISION = 1e18;
+
+    TSwapPoolHandler handler;
 
     function setUp() public {
         weth = new ERC20Mock();
@@ -26,30 +27,47 @@ contract Invariant is StdInvariant, Test {
         factory = new PoolFactory(address(weth));
         pool = TSwapPool(factory.createPool(address(poolToken)));
 
-        // Create those initial x & y balances
+        // Create the initial x & y values for the pool
         poolToken.mint(address(this), uint256(STARTING_X));
         weth.mint(address(this), uint256(STARTING_Y));
-
         poolToken.approve(address(pool), type(uint256).max);
         weth.approve(address(pool), type(uint256).max);
-
-        // Deposit into the pool, give the starting X & Y balances
         pool.deposit(uint256(STARTING_Y), uint256(STARTING_Y), uint256(STARTING_X), uint64(block.timestamp));
 
-        handler = new Handler(pool);
+        handler = new TSwapPoolHandler(pool);
+
         bytes4[] memory selectors = new bytes4[](2);
-        selectors[0] = Handler.deposit.selector;
-        selectors[1] = Handler.swapPoolTokenForWethBasedOnOutputWeth.selector;
+        selectors[0] = TSwapPoolHandler.deposit.selector;
+        selectors[1] = TSwapPoolHandler.swapPoolTokenForWethBasedOnOutputWeth.selector;
 
         targetSelector(FuzzSelector({ addr: address(handler), selectors: selectors }));
         targetContract(address(handler));
     }
 
-    function statefulFuzz_constantProductFormulaStaysTheSameX() public view {
+    // Normal Invariant
+    // x * y = k
+    // x * y = (x + ∆x) * (y − ∆y)
+    // x = Token Balance X
+    // y = Token Balance Y
+    // ∆x = Change of token balance X
+    // ∆y = Change of token balance Y
+    // β = (∆y / y)
+    // α = (∆x / x)
+
+    // Final invariant equation without fees:
+    // ∆x = (β/(1-β)) * x
+    // ∆y = (α/(1+α)) * y
+
+    // Invariant with fees
+    // ρ = fee (between 0 & 1, aka a percentage)
+    // γ = (1 - p) (pronounced gamma)
+    // ∆x = (β/(1-β)) * (1/γ) * x
+    // ∆y = (αγ/1+αγ) * y
+    function invariant_deltaXFollowsMath() public {
         assertEq(handler.actualDeltaX(), handler.expectedDeltaX());
     }
 
-    function statefulFuzz_constantProductFormulaStaysTheSameY() public view {
+    function invariant_deltaYFollowsMath() public {
         assertEq(handler.actualDeltaY(), handler.expectedDeltaY());
     }
 }
